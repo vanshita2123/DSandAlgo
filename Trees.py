0507312851bd4022718288e103578961bfb8dca9
yukti845 <yukti845@gmail.com>
2020-09-10 21:53:17 +0530

    update

---
+++ Trees.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self,data):
+        new_node = Node(data)
+        if self.root == None:
+            self.root = new_node
+            return
+        else:
+            curr_node = self.root
+            while True:
+                if data < curr_node.data:
+                    if curr_node.left == None:
+                        curr_node.left = new_node
+                        return
+                    else:
+                        curr_node = curr_node.left
+                elif data > curr_node.data:
+                    if curr_node.right == None:
+                        curr_node.right = new_node
+                        return
+                    else:
+                        curr_node = curr_node.right
+
+    def lookup(self,data):
