d16636400dfc49efdc2bb13bbb106b4e155e1f9e
yukti845 <yukti845@gmail.com>
2020-09-10 21:22:06 +0530

    update

---
+++ Stacks.py
@@ -0,0 +1,87 @@
+# Stacks
+# Stacks using linked lists
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def peek(self):
+        return self.top.data
+
+    def push(self,data):
+        new_node = Node(data)
+        if self.bottom == None:
+            self.bottom = new_node
+            self.top = new_node
+            self.top = self.bottom
+            self.length = 1
+        else:
+            new_node.next = self.top
+            self.top = new_node
+            self.length+=1
+
+    def pop(self):
+        if not self.top:
+            return None
+        holderPointer = self.top
+        self.top=self.top.next
+        self.length-=1
+        if self.length==0:
+            self.bottom = None
+        return holderPointer.data
+
+    def printt(self):
+        temp = self.top
+        while temp!=None:
+            print(temp.data, end=" --> ")
+            temp = temp.next
+        print()
+
+mystack = Stack()
+mystack.push('google')
+mystack.push('microsoft')
+mystack.push('facebook')
+mystack.push('apple')
+mystack.printt()
+x = mystack.peek()
+print(x)
+y = mystack.pop()
+print(y)
+mystack.printt()
+qw = mystack.peek()
+print(qw)
+# Stacks using arrays
+class Stack:
+    def __init__(self):
+        self.arr = []
+        self.length = 0
+
+    def peek(self):
+        return self.arr[self.length-1]
+
+    def push(self,value):
+        self.arr.append(value)
+        self.length+=1
+
+    def pop(self):
+        popped_item = self.arr[self.length-1]
+        del self.arr[self.length-1]
+        self.length-=1
+        return popped_item
+
+mystack = Stack()
+mystack.push('google')
+mystack.push('microsoft')
+mystack.push('facebook')
+mystack.push('apple')
+print(mystack)
+x = mystack.peek()
+print(x)
+mystack.pop()
+print(mystack)
