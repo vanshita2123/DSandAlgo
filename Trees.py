85e4faa8f9e2d3c1f89cd40f5bb218ba539fd0c5
yukti845 <yukti845@gmail.com>
2020-09-10 23:17:37 +0530

    update

--- Trees.py
+++ Trees.py
@@ -30,3 +30,32 @@ class BinarySearchTree:
                         curr_node = curr_node.right
 
     def lookup(self,data):
+        curr_node = self.root
+        while True:
+            if curr_node == None:
+                return False
+            if curr_node.data == data:
+                return True
+            elif data < curr_node.data:
+                curr_node = curr_node.left
+            else:
+                curr_node = curr_node.right
+
+    def remove:
+        if self.root == None:
+            return False
+        curr_node = self.root
+        parent_node = None
+        while True:
+            if data < curr_node.data:
+                parent_node = curr_node
+                curr_node = curr_node.left
+            elif data > curr_node.data:
+                parent_node = curr_node
+                curr_node = curr_node.right
+            elif curr_node.data == data:
+
+                if curr_node.right == None:
+                    if parent_node == None:
+                        self.root = curr_node.left
+                    
