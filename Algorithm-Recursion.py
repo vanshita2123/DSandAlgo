2c2190f1385bdeac1a6b3c95368d8297dfa67fce
yukti845 <yukti845@gmail.com>
2020-09-17 01:11:22 +0530

    update

---
+++ Algorithm-Recursion.py
@@ -0,0 +1,15 @@
+# Factorial of a number
+# Iterative
+def fact(num):
+    result = 1
+    for i in range(1, num+1):
+        result = result * i
+    return result
+
+def factorial(number):
+    if number == 1:
+        return 1
+    return number * factorial(number - 1)
+
+print(fact(4))
+print(factorial(5))
