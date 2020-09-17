e8fa3793ffe1e22c2aa6217e45099a7a8a71eb51
yukti845 <yukti845@gmail.com>
2020-09-17 18:33:54 +0530

    update

--- Algorithm-Recursion.py
+++ Algorithm-Recursion.py
@@ -15,3 +15,24 @@ def factorial(number):
 
 print(fact(4))
 print(factorial(5))
+
+# Fibonacci
+# Iterative approach
+def fibonacci_iterative(num):
+    a = 0
+    b = 1
+    sum = 0
+    for i in range(num):
+        sum = a+b
+        a = b
+        b = sum
+    return sum
+
+# Recursive approach
+def fibonacci_recursive(num):
+    if num < 2:
+        return num
+    return fibonacci_recursive(num - 1) + fibonacci_recursive(num - 2)
+
+print(fibonacci_recursive(4))
+print(fibonacci_iterative(4))
