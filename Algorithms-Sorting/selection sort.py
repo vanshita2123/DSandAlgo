e3bff6d2c6b11ededd3ea850229e0a00dbed5d1d
yukti845 <yukti845@gmail.com>
2020-09-18 20:05:59 +0530

    update

---
+++ Algorithms-Sorting/selection sort.py
@@ -0,0 +1,15 @@
+def selection_sort(arr):
+     i = 0
+     while i < len(arr):
+         min = a[i]
+         index = i
+         for j in range(i+1, len(arr)):
+             if a[j] < min:
+                 min = a[j]
+                 index = j
+         arr[i], arr[index] = arr[index], arr[i]
+         i+=1
+     return arr
+
+a = [32, 84, 30, 21, 4, 90, 65, 47, 1, 36, 77]
+print(selection_sort(a))
