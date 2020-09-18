7fe6d5ff3d6fe6c42660b9ed9f6ca527749fc020
yukti845 <yukti845@gmail.com>
2020-09-18 19:31:34 +0530

    update

---
+++ Algorithms-Sorting/Bubble sortt.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    s = len(arr)
+    for i in range(s):
+        for j in range(s-1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+    return arr
+
+a = [37, 82, 49, 2, 13, 90, 56, 8, 63, 21]
+print(bubble_sort(a))
+
+
