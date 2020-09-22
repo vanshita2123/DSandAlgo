98bb20548273bbe157498099c369ffebdbe14349
yukti845 <yukti845@gmail.com>
2020-09-22 20:11:34 +0530

    update

--- Algorithm-Searching/Breadth first search.py
+++ Algorithm-Searching/Breadth first search.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, val):
+        self.val = val
         self.left = None
         self.right = None
 
@@ -8,25 +8,23 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, data):
-        new_node = Node(data)
+    def insert(self, val):
+        new_node = Node(val)
         if self.root == None:
             self.root = new_node
             return
-        else:
-            curr_node = self.root
-            while True:
-                if data < curr_node.data:
-                    if curr_node.left == None:
-                        curr_node.left = new_node
-                        return
-                    else:
-                        curr_node = curr_node.left
-
-                elif data > curr_node.data:
-                    if curr_node.right == None:
-                        curr_node.right = new_node
-                        return
-                    else:
-                        curr_node = curr_node.right
-                        
+        temp = self.root
+        while True:
+            if new_node.val < temp.val:
+                if temp.left == None:
+                    temp.left = new_node
+                    break
+                else:
+                    temp = temp.left
+            elif new_node.val > temp.val:
+                if temp.right == None:
+                    temp.right = new_node
+                    break
+                else:
+                    temp = temp.right
+                    
